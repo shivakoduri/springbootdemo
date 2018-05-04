78b80a6311b568119c412255b5928fb491bf5fb0
shivakoduri <koduri5782>
2018-05-04 10:28:26 +0800

    Initial commit for spring boot 2 project structure

---
+++ src/main/java/com/myprojects/examples/springboot/SpringBootParentApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.examples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootParentApplication.class, args);
+	}
+}
